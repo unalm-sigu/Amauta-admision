55f66b08584d68af53456c5c9bf389d589dee42f
Diego <diego@albatross.pe>
2016-11-04 11:22:10 -0500

    Modulo de carga academica

--- src/main/webapp/public/app/academico/docente/cargaacademica/cargaAcademica.js
+++ src/main/webapp/public/app/academico/docente/cargaacademica/cargaAcademica.js
@@ -1,36 +1,44 @@
 $(function () {
-    SistemaCalificacion = {
-        verSistemaCurso: function ($this, e) {
-            e.preventDefault();
+    var dynatable = $('#dynaTable').dynatable({
+        dataset: {
+            ajaxUrl: APP.url('academico/docente/cargaacademica/list'),
+            perPageDefault: 100
+        },
+        writers: {
+            _rowWriter: ulWriter
+        },
+        table: {
+            bodyRowSelector: 'tbody tr'
+        }
+    }).data('dynatable');
 
+    function ulWriter(rowIndex, record, columns, cellWriter) {
+        var colorEstado = {ACT: "success", CER: "danger", CRE: "default"};
+        record.colorEstado = colorEstado[record.estado];
+        record.index = rowIndex;
+
+        var html = $.templates("#templateCargaAcademica").render(record);
+        return html;
+    }
+
+    CargaAcademica = {
+        aceptarSistemaCalificacion: function ($this, e) {
+            e.preventDefault();
+            var tr = $this.closest("tr");
+            var idx = tr.attr("rel");
+            var rec = dynatable.settings.dataset.records[idx];
+
+            MODAL.hide();
             MODAL.init("lg");
-            MODAL.title("Sistema de Calificación - SC-003");
+            MODAL.title("Sistema de Calificación " + rec.sistemaCalificacion);
             MODAL.show();
             MODAL.buttons(
                     '<a class="btn btn-success">Aceptar</a>' +
                     '<a class="btn btn-warning">Expandir</a>' +
                     '<a class="btn btn-danger">Solicita modificación</a>');
-            $.ajax({
-                url: APP.url('academico/docente/carga/detalleCargaAcademica'),
-                type: 'POST',
-                async: false,
-                success: function (response) {
-                    MODAL.body(response);
-                },
-                error: function () {
-                    notify(MESSAGES.errorComunicacion, "error");
-                }
-            });
-        },
-        verSistemaCursoModal2: function ($this, e) {
-            e.preventDefault();
 
-            MODAL.init("lg");
-            MODAL.title("Sistema de Calificación - SC-003");
-            MODAL.show();
-           
             $.ajax({
-                url: APP.url('academico/docente/carga/detalleCargaAcademica'),
+                url: APP.url('academico/docente/cargaacademica/' + rec.idSistemaCalificacion + '/detalleSistemaCalificacion'),
                 type: 'POST',
                 async: false,
                 success: function (response) {
@@ -41,11 +49,34 @@ $(function () {
                 }
             });
         },
+        verSistemaCalificacion: function ($this, e) {
+            e.preventDefault();
+            var tr = $this.closest("tr");
+            var idx = tr.attr("rel");
+            var rec = dynatable.settings.dataset.records[idx];
+
+            MODAL.hide();
+            MODAL.init("lg");
+            MODAL.title("Detalle del Sistema de Calificación - " + rec.sistemaCalificacion);
+            MODAL.show();
+
+            $.ajax({
+                url: APP.url('academico/docente/cargaacademica/' + rec.idSistemaCalificacion + '/detalleSistemaCalificacion'),
+                type: 'POST',
+                async: false,
+                success: function (response) {
+                    MODAL.body(response);
+                },
+                error: function () {
+                    notify(MESSAGES.errorComunicacion, "error");
+                }
+            });
+        }
     };
-    $("body").delegate(".sistema-curso", "click", function (e) {
-        SistemaCalificacion.verSistemaCurso($(this),e);
+    $("body").delegate(".aceptar-sistema-calificacion", "click", function (e) {
+        CargaAcademica.aceptarSistemaCalificacion($(this), e);
     });
-    $("body").delegate(".sistema-curso-m2", "click", function (e) {
-        SistemaCalificacion.verSistemaCursoModal2($(this),e);
+    $("body").delegate(".sistema-calificacion", "click", function (e) {
+        CargaAcademica.verSistemaCalificacion($(this), e);
     });
 });
