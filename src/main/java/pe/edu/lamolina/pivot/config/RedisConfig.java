2d8f566ba2e0b6eee9b536ce0e78ceeb1ae7561c
José EA <jose@albatross.pe>
2016-12-14 16:40:37 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/config/RedisConfig.java
+++ src/main/java/pe/edu/lamolina/pivot/config/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class RedisConfig {
 
     @Bean
