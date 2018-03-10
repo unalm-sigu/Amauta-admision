87389635d4d3fc77f14387ad8d3f9141a786170b
dpinedac <david@albatross.pe>
2018-03-09 19:22:31 -0500

    interceptor

--- src/main/java/pe/edu/lamolina/pivot/config/DespliegueConfig.java
+++ src/main/java/pe/edu/lamolina/pivot/config/DespliegueConfig.java
@@ -9,84 +9,95 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "despliegue")
 public class DespliegueConfig {
-    
+
     @Value("${prospecto}")
     String prospecto;
-    
+
     @Value("${mailer}")
     Boolean mailer;
-    
+
     @Value("${lagunas}")
     Boolean lagunas;
-    
+
     @Value("${emails}")
     String emails;
-    
+
     @Value("${copias}")
     String copias;
-    
+
     @Value("${s3}")
     Boolean s3;
-    
+
     @Value("${tawkto}")
     Boolean tawkto;
-    
+
+    @Value("${sistema}")
+    Long sistema;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
+    public Long getSistema() {
+        return sistema;
+    }
+
+    public void setSistema(Long sistema) {
+        this.sistema = sistema;
+    }
+
     public String getProspecto() {
         return prospecto;
     }
-    
+
     public void setProspecto(String prospecto) {
         this.prospecto = prospecto;
     }
-    
+
     public Boolean getMailer() {
         return mailer;
     }
-    
+
     public void setMailer(Boolean mailer) {
         this.mailer = mailer;
     }
-    
+
     public String getEmails() {
         return emails;
     }
-    
+
     public void setEmails(String emails) {
         this.emails = emails;
     }
-    
+
     public String getCopias() {
         return copias;
     }
-    
+
     public void setCopias(String copias) {
         this.copias = copias;
     }
-    
+
     public Boolean getLagunas() {
         return lagunas;
     }
-    
+
     public void setLagunas(Boolean lagunas) {
         this.lagunas = lagunas;
     }
-    
+
     public Boolean getS3() {
         return s3;
     }
-    
+
     public void setS3(Boolean s3) {
         this.s3 = s3;
     }
-    
+
     public Boolean getTawkto() {
         return tawkto;
     }
-    
+
     public void setTawkto(Boolean tawkto) {
         this.tawkto = tawkto;
     }
-    
+
 }
--- src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorService.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorService.java
@@ -2,8 +2,9 @@ package pe.edu.lamolina.pivot.controller.interceptor;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public interface InterceptorService {
 
-    void saveInterceptor (HttpServletRequest servlet, ObjectNode objNode);
+    void saveInterceptor (HttpServletRequest servlet, ObjectNode objNode,HttpSession session);
 }
