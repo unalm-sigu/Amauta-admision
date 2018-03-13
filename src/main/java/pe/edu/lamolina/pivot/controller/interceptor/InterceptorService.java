30211ad0c84ebd9676a7a7ff4a12409d33b439f9
dpinedac <david@albatross.pe>
2018-03-13 14:20:37 -0500

    interceptor

--- src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorService.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorService.java
@@ -1,10 +1,9 @@
 package pe.edu.lamolina.pivot.controller.interceptor;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 public interface InterceptorService {
 
-    void saveInterceptor (HttpServletRequest servlet, ObjectNode objNode,HttpSession session);
+    void saveInterceptor (ObjectNode objNode,HttpSession session);
 }
--- src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorServiceImpl.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/interceptor/InterceptorServiceImpl.java
@@ -3,10 +3,11 @@ package pe.edu.lamolina.pivot.controller.interceptor;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.Arrays;
 import java.util.Date;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import pe.edu.lamolina.model.interceptor.UserLogger;
 import pe.edu.lamolina.model.seguridad.Sistema;
@@ -26,12 +27,14 @@ public class InterceptorServiceImpl implements InterceptorService {
     @Autowired
     DespliegueConfig despliegueConfig;
 
+    @Async
     @Override
-    @Transactional
-    public void saveInterceptor(HttpServletRequest servlet, ObjectNode objNode, HttpSession session) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveInterceptor(ObjectNode objNode, HttpSession session) {
         DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
+        System.err.println("SERVLET --->");
         UserLogger obj = new UserLogger();
-        String [] ls = new String[ds.getRoles().size()];
+        String[] ls = new String[ds.getRoles().size()];
         int i = 0;
         for (Rol role : ds.getRoles()) {
             ls[i] = role.getCodigo();
@@ -40,9 +43,9 @@ public class InterceptorServiceImpl implements InterceptorService {
         Sistema objSis = new Sistema();
         objSis.setId(despliegueConfig.getSistema());
         obj.setAccion(objNode.get("tipo").toString());
-        obj.setBrowser(servlet.getHeader("User-Agent"));
-        obj.setDireccionIp(servlet.getRemoteAddr());
-        obj.setSistemaOperativo(getClientOS(servlet));
+        obj.setBrowser(ds.getBrowser());
+        obj.setDireccionIp(ds.getDireccionIp());
+        obj.setSistemaOperativo(ds.getSistemaOperativo());
         obj.setFechaRegistro(new Date());
         obj.setData(objNode.get("data").toString());
         obj.setCiclo(ds.getCicloAcademico().getDescripcion());
@@ -53,23 +56,4 @@ public class InterceptorServiceImpl implements InterceptorService {
 
     }
 
-    public String getClientOS(HttpServletRequest request) {
-        final String browserDetails = request.getHeader("User-Agent");
-
-        //=================OS=======================
-        final String lowerCaseBrowser = browserDetails.toLowerCase();
-        if (lowerCaseBrowser.contains("windows")) {
-            return "Windows";
-        } else if (lowerCaseBrowser.contains("mac")) {
-            return "Mac";
-        } else if (lowerCaseBrowser.contains("x11")) {
-            return "Unix";
-        } else if (lowerCaseBrowser.contains("android")) {
-            return "Android";
-        } else if (lowerCaseBrowser.contains("iphone")) {
-            return "IPhone";
-        } else {
-            return "UnKnown, More-Info: " + browserDetails;
-        }
-    }
 }
