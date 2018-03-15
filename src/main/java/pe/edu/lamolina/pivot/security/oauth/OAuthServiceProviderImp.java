6ade8914c02981ecfe75523e9c8a408ff25c8058
dpinedac <david@albatross.pe>
2018-03-15 16:19:21 -0500

    cambios

--- src/main/java/pe/edu/lamolina/pivot/security/oauth/OAuthServiceProviderImp.java
+++ src/main/java/pe/edu/lamolina/pivot/security/oauth/OAuthServiceProviderImp.java
@@ -33,6 +33,7 @@ import pe.edu.lamolina.model.general.Oficina;
 import pe.edu.lamolina.model.seguridad.Rol;
 import pe.edu.lamolina.model.seguridad.Usuario;
 import pe.edu.lamolina.model.enums.TipoSeccionEnum;
+import pe.edu.lamolina.model.enums.TipoSesionEnum;
 import pe.edu.lamolina.pivot.controller.interceptor.InterceptorService;
 import pe.edu.lamolina.pivot.dao.academico.CarreraDAO;
 import pe.edu.lamolina.pivot.dao.academico.CicloAcademicoDAO;
@@ -189,7 +190,7 @@ public class OAuthServiceProviderImp implements OAuthServiceProvider {
         ObjectNode objData = new ObjectNode(JsonNodeFactory.instance);
         obj.put("usuario", ds.getUsuario().getPersona().getNombreCompleto());
         objData.put("data", obj);
-        objData.put("tipo", "Logout");
+        objData.put("tipo", TipoSesionEnum.LOGIN.name());
         interceptorService.saveInterceptor(objData, session);
     }
  public String getClientOS(HttpServletRequest request) {
