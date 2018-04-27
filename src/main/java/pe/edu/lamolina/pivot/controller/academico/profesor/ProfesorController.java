e013e2f05d403777f6f5b1315133bd13456fcf2c
bladymir <bladymir@albatross.pe>
2018-04-27 17:55:19 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/profesor/ProfesorController.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/profesor/ProfesorController.java
@@ -56,7 +56,7 @@ import pe.edu.lamolina.model.academico.Seccion;
 import pe.edu.lamolina.model.enums.EstadoPlanCalificaEnum;
 import pe.edu.lamolina.model.general.Compania;
 import pe.edu.lamolina.model.general.Persona;
-import pe.edu.lamolina.pivot.controller.academico.cargaacademica.CargaAcademicaService;
+import pe.edu.lamolina.pivot.controller.academico.notasacademicas.CargaAcademicaService;
 import pe.edu.lamolina.pivot.controller.academico.visitante.AlumnoHelper;
 import pe.edu.lamolina.pivot.controller.general.foto.FotoHelper;
 import pe.edu.lamolina.pivot.zelper.constant.Constantine;
