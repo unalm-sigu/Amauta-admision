5bac80ec35e62a8aeae4f973688b550ac10858a9
lyon666 <lyon@albatross.pe>
2018-05-08 18:41:28 -0500

    fix

--- src/main/resources/public/app/tramite/updatehistorialacademico/updateHistorialAcademico.js
+++ src/main/resources/public/app/tramite/updatehistorialacademico/updateHistorialAcademico.js
@@ -87,7 +87,7 @@ new Vue({
             }
             $.ajax({
                 method: 'POST',
-                url: APP.url('tramite/solicitudconstancia/updatehistorial/updatehistorial'),
+                url: APP.url('tramite/solicitudconstancia/updatehistorial/savehistorial'),
                 data: $('#form').serialize(),
                 success: function(response) {
                     if (response.success) {
