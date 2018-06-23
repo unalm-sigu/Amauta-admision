5af73f434947b128c292b18add01b1d3f6a8f45e
lyon666 <lyon@albatross.pe>
2018-06-23 18:31:05 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
+++ src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
@@ -1,58 +1,27 @@
 package pe.edu.lamolina.pivot.zelper;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import pe.edu.lamolina.model.academico.Alumno;
+import pe.edu.lamolina.model.academico.CicloAcademico;
+import pe.edu.lamolina.model.academico.MatriculaResumen;
 
 public class Laboratory {
-
+    
     public static void main666(String[] args) {
-
-        String conte = "__AULA__<p style=\"text-align:right\">__NUMERO__-DRAD/__SERIE__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<h1><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; __NOMBRE__</strong></h1>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">Con matr&iacute;cula N&ordm; __CODIGOALUMNO__, es __ALUMNO__ de la Facultad de <strong>__FACULTAD__</strong>, ha realizado estudios en dicha Facultad desde el __CICLOINICIOROMANO_ ciclo de __YEARINICIOCICLO__al __CICLOFINROMANO_ de __YEARFINCICLO__, en forma ininterrumpida.</p>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">Se encuentra __MATRICULADO__ en el ciclo _CICLOACTUAL__.</p>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">__FECHA__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">__JEFEOFICINA__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>";
-
-        String partes[] = conte.split("__");
-        Map<String, String> mapVariables = new LinkedHashMap();
-        for (String parte : partes) {
-            if (isAlpha(parte)) {
-                String variable = "__" + parte + "__";
-                if (conte.contains(variable)) {
-                    mapVariables.put(variable, variable);
-                }
-            }
+        ObjectMapper mapper = new ObjectMapper();
+        Alumno alu = new Alumno(45234);
+        alu.setCicloIngreso(new CicloAcademico(33));
+        alu.setMatriculaResumen(new ArrayList());
+        alu.getMatriculaResumen().add(new MatriculaResumen(10));
+        alu.getMatriculaResumen().add(new MatriculaResumen(12));
+        
+        try {
+            String jsonInString = mapper.writeValueAsString(alu);
+            System.out.println(jsonInString);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
         }
-
-        for (Map.Entry<String, String> entry : mapVariables.entrySet()) {
-            System.out.println(entry.getKey());
-        }
-
-    }
-
-    public static boolean isAlpha(String name) {
-        return name.matches("[0-9A-Z]+");
     }
 }
