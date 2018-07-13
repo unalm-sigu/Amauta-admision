ae35f3874063748d57a15acffcfbe99202f6199b
lyon666 <lyon@albatross.pe>
2018-07-13 16:36:54 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
+++ src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
@@ -1,58 +1,38 @@
 package pe.edu.lamolina.pivot.zelper;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.apache.commons.lang3.text.WordUtils;
 
 public class Laboratory {
 
     public static void main666(String[] args) {
 
-        String conte = "__AULA__<p style=\"text-align:right\">__NUMERO__-DRAD/__SERIE__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:right\">&nbsp;</p>\n"
-                + "\n"
-                + "<h1><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; __NOMBRE__</strong></h1>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">Con matr&iacute;cula N&ordm; __CODIGOALUMNO__, es __ALUMNO__ de la Facultad de <strong>__FACULTAD__</strong>, ha realizado estudios en dicha Facultad desde el __CICLOINICIOROMANO_ ciclo de __YEARINICIOCICLO__al __CICLOFINROMANO_ de __YEARFINCICLO__, en forma ininterrumpida.</p>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">Se encuentra __MATRICULADO__ en el ciclo _CICLOACTUAL__.</p>\n"
-                + "\n"
-                + "<p style=\"text-align:justify\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">__FECHA__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">__JEFEOFICINA__</p>\n"
-                + "\n"
-                + "<p style=\"text-align:center\">&nbsp;</p>";
+        String nom = "Juan cArlis\td'OnofriO, jesús ÑOÑO 3453453 françois \n"
+                + "   tudela-quispe";
+        System.out.println(WordUtils.capitalize(nom.toLowerCase()));
 
-        String partes[] = conte.split("__");
-        Map<String, String> mapVariables = new LinkedHashMap();
-        for (String parte : partes) {
-            if (isAlpha(parte)) {
-                String variable = "__" + parte + "__";
-                if (conte.contains(variable)) {
-                    mapVariables.put(variable, variable);
-                }
-            }
+        nom = nom.toLowerCase();
+        nom = nom.replaceAll("[^a-zçñáéíóúü\\s'\\-]", "");
+        nom = nom.replaceAll("[\\n\\r|,\\t]", " ");
+        nom = nom.replaceAll(" +", " ");
+        String[] arr = nom.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
         }
+        nom = String.join(" ", arr);
 
-        for (Map.Entry<String, String> entry : mapVariables.entrySet()) {
-            System.out.println(entry.getKey());
+        arr = nom.split("'");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
         }
+        nom = String.join("'", arr);
 
-    }
+        arr = nom.split("-");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+        }
+        nom = String.join("-", arr);
 
-    public static boolean isAlpha(String name) {
-        return name.matches("[0-9A-Z]+");
+        System.out.println(nom);
+
     }
 }
