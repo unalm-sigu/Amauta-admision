a220d63245b3ee2c13ff4f37816aac42f128de4b
bladymir <bladymircch@gmail.com>
2018-08-06 14:06:36 -0500

    add relacionar acarggo relacionar funcion

--- src/main/resources/public/app/seguridad/rol/rolSistema.js
+++ src/main/resources/public/app/seguridad/rol/rolSistema.js
@@ -1,14 +1,27 @@
 Vue.component("multiselect", window.VueMultiselect.default);
 
 new Vue({
-    el: '#pageRolSistemaVUE',
+    el: '#main',
+    mixins: [Vudal],
     data: {
-        rolesUrl: APP.url('seguridad/rol/list')
+        rolesUrl: APP.url('seguridad/rol/list'),
+        cargo: {id: null},
     },
     computed: {
     },
     mounted() {
     },
     methods: {
+        relacionarCargo: function (rol) {
+            let vue = this;
+            vue.vudalOpen();
+        },
+        relacionarFuncion: function (rol) {
+       
+        },
+        saveCargo: function () {
+            let vue = this;
+            vue.vudalClose();
+        }
     }
 });
