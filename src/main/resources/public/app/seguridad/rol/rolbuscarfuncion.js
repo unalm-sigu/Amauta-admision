7db8d253f3a179d929975851ffbcc6e49f39f7a0
bladymir <bladymircch@gmail.com>
2018-08-07 17:17:18 -0500

    add modulo roles sistema agregar cargo y funcion

---
+++ src/main/resources/public/app/seguridad/rol/rolbuscarfuncion.js
@@ -0,0 +1,54 @@
+Vue.component("funcionsearh", {
+    template: "#funcionsearhtemp",
+    props: {
+        funcion: {id: null},
+        nombre: {type: String, default: ''},
+    },
+    mounted: function () {
+        let vue = this;
+        let self = $(vue.$el);
+        self.select2(vue.buscar(vue));
+    },
+    updated: function () {
+        let vue = this;
+        let self = $(vue.$el);
+        self.select2(vue.buscar(vue));
+    },
+    methods: {
+        buscar: function (vue) {
+            return {
+                minimumInputLength: 2,
+                allowClear: true,
+                placeholder: " ",
+                ajax: {
+                    url: APP.url("seguridad/rol/allperfilcompania"),
+                    dataType: 'json',
+                    type: 'post',
+                    data: function (term, page) {
+                        return {nombre: term, page: page,tipo:'PERFIL'};
+                    },
+                    results: function (response, page) {
+                        return {results: response.data};
+                    }
+                },
+                initSelection: function (element, callback) {
+                    if (vue.funcion.id != null) {
+                        callback(vue.funcion);
+                    }
+                },
+                formatResult: function (info) {
+                    return info.codigo + ' - ' + info.nombre;
+                },
+                formatSelection: function (info) {
+                    vue.funcion.nombre = info.nombre;
+                    vue.funcion.codigo = info.codigo;
+                    vue.funcion.id = info.id;
+                    return info.nombre;
+                },
+                escapeMarkup: function (m) {
+                    return m;
+                }
+            };
+        }
+    }
+});
