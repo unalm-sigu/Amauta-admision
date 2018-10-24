ba321831a53ebf61917a5a7069b3aad14f4d0b45
dcalmla01 <dcalmla01@gmail.com>
2018-10-24 17:11:28 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/preciocursociclo/PrecioCursoCicloServiceImp.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/preciocursociclo/PrecioCursoCicloServiceImp.java
@@ -1,6 +1,5 @@
 package pe.edu.lamolina.pivot.controller.academico.preciocursociclo;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
