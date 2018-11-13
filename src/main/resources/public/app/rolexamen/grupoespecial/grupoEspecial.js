f61246bcccf8bffd6487e2b94ee3934d9f95efb1
diesgut <diego@albatross.pe>
2018-11-13 09:54:42 -0500

    Avance Grupos Regulares 60%

--- src/main/resources/public/app/rolexamen/grupoespecial/grupoEspecial.js
+++ src/main/resources/public/app/rolexamen/grupoespecial/grupoEspecial.js
@@ -3,12 +3,21 @@ Vue.component("multiselect", window.VueMultiselect.default);
 new Vue({
     el: '#main',
     data: {
-        URL: APP.url('rolexamen/grupoespecial')
+        URL: APP.url('rolexamen/grupoespecial'),
+        rolesExamenes: JSON.parse(jRolesExamenes),
+        rolExamen: null
     },
     mounted() {
 
     },
     methods: {
-
+        rolExamenCustomLabel( { eventoCicloAcademico }) {
+            if (eventoCicloAcademico == null || eventoCicloAcademico.eventoAcademico == null) {
+                return "";
+            }
+            return `${eventoCicloAcademico.eventoAcademico.nombre}`;
+        }, changeRolExamen() {
+            //  this.listGruposRegulares(this.rolExamen);
+        }
     }
 });
