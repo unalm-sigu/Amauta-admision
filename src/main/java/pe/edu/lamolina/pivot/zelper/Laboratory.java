3fcbe6534611657e46ed0aa6d67c279986c917a3
bladymir <bladymircch@gmail.com>
2018-12-19 10:31:00 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
+++ src/main/java/pe/edu/lamolina/pivot/zelper/Laboratory.java
@@ -1,12 +1,9 @@
 package pe.edu.lamolina.pivot.zelper;
 
-import org.joda.time.DateTime;
-
 public class Laboratory {
 
     public static void main666(String[] args) {
-        String da = "002456";
-        System.err.println(da.substring(2));
+
     }
 
 }
