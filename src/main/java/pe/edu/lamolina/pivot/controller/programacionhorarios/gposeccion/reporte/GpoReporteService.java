51fc8d895f726415487460a3e5ba81c552abaf62
bladymir <bladymircch@gmail.com>
2019-01-04 19:10:48 -0500

    add reporte pago verano

---
+++ src/main/java/pe/edu/lamolina/pivot/controller/programacionhorarios/gposeccion/reporte/GpoReporteService.java
@@ -0,0 +1,18 @@
+package pe.edu.lamolina.pivot.controller.programacionhorarios.gposeccion.reporte;
+
+import java.util.List;
+import pe.edu.lamolina.model.academico.CicloAcademico;
+import pe.edu.lamolina.model.academico.DepartamentoAcademico;
+import pe.edu.lamolina.pivot.controller.programacionhorarios.gposeccion.GpoSeccionResumen;
+
+public interface GpoReporteService {
+
+    CicloAcademico findCiclo(CicloAcademico cicloAcademico);
+
+    GpoSeccionResumen resumenByCiclo(CicloAcademico ciclo);
+
+    List<DepartamentoAcademico> allDepartamentoAcademico(CicloAcademico cicloAcademico);
+
+    List<DepartamentoAcademico> allDepartamentoAcademicoXcurso(CicloAcademico cicloAcademico);
+
+}
