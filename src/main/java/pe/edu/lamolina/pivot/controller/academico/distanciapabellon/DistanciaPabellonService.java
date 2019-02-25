d3dcc85c9949b8340e2b5fb3c9953e1ab397cd5b
Bladymir <bladymircch@gmail.com>
2019-02-25 16:38:49 -0500

    fix distancia pabellon

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/distanciapabellon/DistanciaPabellonService.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/distanciapabellon/DistanciaPabellonService.java
@@ -1,13 +1,22 @@
 package pe.edu.lamolina.pivot.controller.academico.distanciapabellon;
 
 import java.util.List;
+import pe.albatross.octavia.dynatable.DynatableFilter;
 import pe.edu.lamolina.model.academico.DepartamentoAcademico;
+import pe.edu.lamolina.model.academico.DistanciaPabellon;
 import pe.edu.lamolina.model.general.Aula;
+import pe.edu.lamolina.pivot.zelper.model.DataSessionPivot;
 
 interface DistanciaPabellonService {
 
     List<DepartamentoAcademico> allDepartamentos();
 
     List<Aula> allModulos();
+
+    List<DepartamentoAcademico> allDynatableFilter(DynatableFilter filter, DataSessionPivot ds);
+
+    List<DistanciaPabellon> allDistancia(DepartamentoAcademico departamentoAcademico);
+
+    void saveDistancia(DepartamentoAcademico departamentoAcademico);
 
 }
---
+++ src/main/resources/public/app/academico/distanciapabellon/distanciapabellon.js
@@ -0,0 +1,68 @@
+new Vue({
+    el: '#main',
+    mixins: [VueLoader],
+    data: {
+        urlpavellon: APP.url('academico/distanciapabellon/list'),
+        modalNuevaDistanciaPabellon: {
+            id: 'idModalNuevaDistanciaPabellon',
+            header: true,
+            title: 'Distancia Pabellón'
+        },
+        distancias: [],
+        departamentoSelecto: {},
+    },
+    mounted: function () {
+        let $vue = this;
+    },
+    updated: function () {
+        $(".numerico").numeric({negative: false});
+    },
+    methods: {
+        configurarDistancia(departamento) {
+            let $vue = this;
+            $vue.showLoader();
+            $vue.departamentoSelecto.id = departamento.id;
+            $vue.departamentoSelecto.nombre = departamento.nombre;
+            $.ajax({
+                method: 'POST',
+                url: APP.url('academico/distanciapabellon/allDistancia'),
+                data: {id: departamento.id},
+                success: function (response) {
+                    if (response.success) {
+                        $vue.distancias = response.data;
+                        $vue.$refs.modalNuevaDistanciaPabellon.open();
+                    } else {
+                        notify(response.message, 'error');
+                    }
+                    $vue.hideLoader();
+                }, error: function () {
+                    notify(MESSAGES.errorComunicacion, "error");
+                    $vue.hideLoader();
+                }
+            });
+        },
+        saveDistanciaPabellon() {
+            let $vue = this;
+            $vue.showLoader();
+            $vue.departamentoSelecto.distanciaPabellon = $vue.distancias;
+            $.ajax({
+                method: 'POST',
+                url: APP.url('academico/distanciapabellon/saveDistancia'),
+                data: JSON.stringify($vue.departamentoSelecto),
+                contentType: "application/json",
+                success: function (response) {
+                    if (response.success) {
+                        $vue.distancias = response.data;
+                        $vue.$refs.modalNuevaDistanciaPabellon.close();
+                    } else {
+                        notify(response.message, 'error');
+                    }
+                    $vue.hideLoader();
+                }, error: function () {
+                    notify(MESSAGES.errorComunicacion, "error");
+                    $vue.hideLoader();
+                }
+            });
+        }
+    }
+});
