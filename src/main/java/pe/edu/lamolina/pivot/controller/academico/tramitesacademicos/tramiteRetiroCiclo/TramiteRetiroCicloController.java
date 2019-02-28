42c42d14ba1f40f9b2581545a1e3afc29f977bd8
dpcanales <davd.1491@gmail.com>
2019-02-28 10:02:26 -0500

    cambios

---
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/tramitesacademicos/tramiteRetiroCiclo/TramiteRetiroCicloController.java
@@ -0,0 +1,165 @@
+package pe.edu.lamolina.pivot.controller.academico.tramitesacademicos.tramiteRetiroCiclo;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import java.beans.PropertyEditorSupport;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pe.albatross.octavia.dynatable.DynatableFilter;
+import pe.albatross.octavia.dynatable.DynatableResponse;
+import pe.albatross.zelpers.miscelanea.ExceptionHandler;
+import pe.albatross.zelpers.miscelanea.JsonHelper;
+import pe.albatross.zelpers.miscelanea.JsonResponse;
+import pe.albatross.zelpers.miscelanea.PhobosException;
+import pe.edu.lamolina.model.academico.CicloAcademico;
+import pe.edu.lamolina.model.tramite.RetiroCiclo;
+import pe.edu.lamolina.pivot.zelper.constant.Constantine;
+import pe.edu.lamolina.pivot.zelper.model.DataSessionPivot;
+
+@Controller
+@RequestMapping("academico/tramiteretirociclo")
+public class TramiteRetiroCicloController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    TramiteRetiroCicloService service;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        dataBinder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String value) {
+                try {
+                    setValue(new SimpleDateFormat("dd/MM/yyyy").parse(value));
+                } catch (ParseException e) {
+                    setValue(null);
+                }
+            }
+        });
+
+        dataBinder.registerCustomEditor(BigDecimal.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String value) {
+                try {
+                    setValue(new BigDecimal(value.replaceAll(",", "")));
+                } catch (Exception e) {
+                    setValue(null);
+                }
+            }
+        });
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model, HttpSession session) {
+        DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
+        List<CicloAcademico> cicloAcademicos = service.allCiclos(ds.getCicloAcademico());
+        ArrayNode arrayNode = new ArrayNode(JsonNodeFactory.instance);
+        for (CicloAcademico cicloAcademico : cicloAcademicos) {
+            arrayNode.add(JsonHelper.createJson(cicloAcademico, JsonNodeFactory.instance, new String[]{
+                "*"}));
+        }
+        model.addAttribute("ciclos", arrayNode);
+        model.addAttribute("ciclo", ds.getCicloAcademico());
+        return "academico/tramiteRetiroCiclo/tramiteRetiroCiclo";
+    }
+
+    @ResponseBody
+    @RequestMapping("list")
+    public DynatableResponse listTramites(DynatableFilter filter,
+            HttpSession session) {
+        DynatableResponse json = new DynatableResponse();
+        DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
+        try {
+            List<RetiroCiclo> retiroCiclos = service.allByCiclo(ds.getCicloAcademico(), filter);
+            ArrayNode arrayNode = new ArrayNode(JsonNodeFactory.instance);
+            for (RetiroCiclo cicloAcademico : retiroCiclos) {
+                arrayNode.add(JsonHelper.createJson(cicloAcademico, JsonNodeFactory.instance, new String[]{
+                    "*",
+                    "cicloAcademico.*",
+                    "alumno.*",
+                    "alumno.persona.*",
+                    "alumno.persona.tipoDocumento.*",
+                    "alumno.carrera.*",
+                    "alumno.carrera.facultad.*",}));
+            }
+            json.setData(arrayNode);
+            json.setFiltered(filter.getFiltered());
+            json.setTotal(filter.getTotal());
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.setTotal(0);
+        }
+        return json;
+    }
+
+    @ResponseBody
+    @RequestMapping("save")
+    public JsonResponse save(
+            @RequestBody RetiroCiclo retiroCiclo,
+            Model model,
+            HttpSession session) {
+        JsonNodeFactory jsonFactory = JsonNodeFactory.instance;
+        JsonResponse response = new JsonResponse();
+
+        try {
+            DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
+            CicloAcademico cicloAcademico = ds.getCicloAcademico();
+
+            service.save(retiroCiclo, ds);
+
+            response.setMessage("Se guardó satisfactoriamente.");
+            response.setSuccess(Boolean.TRUE);
+
+        } catch (PhobosException e) {
+            ExceptionHandler.handlePhobosEx(e, response);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, response);
+        }
+
+        return response;
+    }
+
+    @ResponseBody
+    @RequestMapping("update")
+    public JsonResponse update(
+            @RequestBody RetiroCiclo retiroCiclo,
+            Model model,
+            HttpSession session) {
+        JsonNodeFactory jsonFactory = JsonNodeFactory.instance;
+        JsonResponse response = new JsonResponse();
+
+        try {
+            DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
+            CicloAcademico cicloAcademico = ds.getCicloAcademico();
+
+            service.update(retiroCiclo, ds);
+
+            response.setMessage("Se actualizó satisfactoriamente.");
+            response.setSuccess(Boolean.TRUE);
+
+        } catch (PhobosException e) {
+            ExceptionHandler.handlePhobosEx(e, response);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, response);
+        }
+
+        return response;
+    }
+}
--- src/main/java/pe/edu/lamolina/pivot/dao/tramite/hibernate/RetiroCicloDAOH.java
+++ src/main/java/pe/edu/lamolina/pivot/dao/tramite/hibernate/RetiroCicloDAOH.java
@@ -1,8 +1,14 @@
 package pe.edu.lamolina.pivot.dao.tramite.hibernate;
 
+import java.util.List;
 import pe.edu.lamolina.pivot.dao.tramite.RetiroCicloDAO;
 import org.springframework.stereotype.Repository;
+import pe.albatross.octavia.Octavia;
+import pe.albatross.octavia.dynatable.DynatableFilter;
+import pe.albatross.octavia.dynatable.DynatableSql;
 import pe.albatross.octavia.easydao.AbstractEasyDAO;
+import pe.edu.lamolina.model.academico.Alumno;
+import pe.edu.lamolina.model.academico.CicloAcademico;
 import pe.edu.lamolina.model.tramite.RetiroCiclo;
 
 @Repository
@@ -11,5 +17,30 @@ public class RetiroCicloDAOH extends AbstractEasyDAO<RetiroCiclo> implements Ret
     public RetiroCicloDAOH() {
         super();
         setClazz(RetiroCiclo.class);
+    }
+
+    @Override
+    public List<RetiroCiclo> allByCiclo(CicloAcademico cicloAcademico, DynatableFilter filter) {
+        DynatableSql sql = new DynatableSql(filter)
+                .from(RetiroCiclo.class, "rc")
+                .join("cicloRegistro cr", "alumno al", "cicloAcademico ca")
+                .left("al.persona per", "al.carrera car", "car.facultad")
+                .searchFields("ca.nombre", "al.estado", "al.codigo", "per.numeroDocIdentidad")
+                .searchComplexField("concat(coalesce(per.paterno,''),' ',coalesce(per.materno,''),' ',coalesce(per.nombres,''))")
+                .searchComplexField("concat(coalesce(per.nombres,''),' ',coalesce(per.paterno,''),' ',coalesce(per.materno,''))")
+                .filter("cr.id", cicloAcademico);
+
+        return all(sql);
+    }
+
+    @Override
+    public RetiroCiclo findByAlumno(Alumno alumno, CicloAcademico ciclo) {
+        Octavia sql = new Octavia()
+                .from(RetiroCiclo.class, "rc")
+                .join("alumno al", "cicloRegistro cr", "cicloAcademico")
+                .filter("al.id", alumno)
+                .filter("cr.id", ciclo);
+
+        return find(sql);
     }
 }
