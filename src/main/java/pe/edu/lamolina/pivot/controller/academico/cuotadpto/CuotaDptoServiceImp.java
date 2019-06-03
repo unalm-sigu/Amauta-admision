5960bb0a6ab122f7318c15a84a5a2d3202348728
HondaSystem <r7sok.18kn@hotmail.com>
2019-06-03 15:51:35 -0500

    Modulo cuota departamento

---
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/cuotadpto/CuotaDptoServiceImp.java
@@ -0,0 +1,69 @@
+package pe.edu.lamolina.pivot.controller.academico.cuotadpto;
+
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pe.albatross.octavia.dynatable.DynatableFilter;
+import pe.albatross.zelpers.miscelanea.TypesUtil;
+import pe.edu.lamolina.model.academico.CicloAcademico;
+import pe.edu.lamolina.model.academico.CuotasGrupoHoras;
+import pe.edu.lamolina.model.horario.GrupoHoras;
+import pe.edu.lamolina.pivot.dao.academico.AnexoBoletinDAO;
+import pe.edu.lamolina.pivot.dao.academico.CuotaGpoHorasDAO;
+import pe.edu.lamolina.pivot.dao.horario.GrupoHorasDAO;
+
+@Service
+@Transactional(readOnly = true)
+public class CuotaDptoServiceImp implements CuotaDptoService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CuotaGpoHorasDAO cuotaGpoHorasDAO;
+
+    @Autowired
+    AnexoBoletinDAO anexoBoletinDAO;
+
+    @Autowired
+    GrupoHorasDAO grupoHorasDAO;
+
+    @Override
+    public List<GrupoHoras> allGrupos() {
+        return grupoHorasDAO.allRegulares();
+    }
+
+    @Override
+    public List<CuotasGrupoHoras> allCuotasGpoHoras(DynatableFilter filter, GrupoHoras grupoHoras, CicloAcademico cicloAcademico) {
+
+        List<CuotasGrupoHoras> cuotas = cuotaGpoHorasDAO.allByDynatableGpoHoras(filter, grupoHoras, cicloAcademico);
+
+        List<AnexoCuotaUtilizadaBean> anexoCuotaUtilizadaBean = cuotaGpoHorasDAO.allByGpoHorasCiclo(grupoHoras, cicloAcademico);
+
+        Map<String, AnexoCuotaUtilizadaBean> mapAnexoUtilizada = TypesUtil.convertListToMap("idAnexo", anexoCuotaUtilizadaBean);
+
+        for (CuotasGrupoHoras cuota : cuotas) {
+
+            AnexoCuotaUtilizadaBean letraUtilizadoFound = mapAnexoUtilizada.get(cuota.getAnexoBoletin().getId());
+//            LetraCuotaUtilizadaBean letraHorasUtilizadoFound = mapLetraHorasUtilizadas.get(cuota.getGrupoHoras().getLetra());
+//            List<LetraCuotaUtilizadaBean> cantidadGrupoFound = TypesUtil.getListNotNull(mapCantidadGrupos.get(cuota.getGrupoHoras().getLetra()));
+
+            cuota.setHorasUtilizadas(letraUtilizadoFound != null ? letraUtilizadoFound.getCantidad() : 0L);
+//            cuota.setGruposUtilizados(letraHorasUtilizadoFound != null ? letraHorasUtilizadoFound.getCantidad() : 0L);
+
+//            String strCantGpos = "";
+//            for (LetraCuotaUtilizadaBean letra : cantidadGrupoFound) {
+//                strCantGpos += strCantGpos.equals("") ? "" : ", ";
+//                strCantGpos += letra.getGrupo() + "(" + letra.getCantidad() + ")";
+//            }
+//            cuota.setDetalleGrupos(strCantGpos);
+        }
+
+        return cuotas;
+
+    }
+
+}
