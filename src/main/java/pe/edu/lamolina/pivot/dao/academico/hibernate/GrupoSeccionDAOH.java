c50b4403e09c10c801bc48496466764aaf22940e
lyon666 <lyon@albatross.pe>
2019-06-07 19:54:55 -0500

    mejoras reordenar-codigo2

--- src/main/java/pe/edu/lamolina/pivot/dao/academico/hibernate/GrupoSeccionDAOH.java
+++ src/main/java/pe/edu/lamolina/pivot/dao/academico/hibernate/GrupoSeccionDAOH.java
@@ -736,10 +736,24 @@ public class GrupoSeccionDAOH extends AbstractEasyDAO<GrupoSeccion> implements G
     }
 
     @Override
-    public void updateCodigo2(GrupoSeccion gpoSecc) {
-        Octavia octavia = Octavia.update(GrupoSeccion.class);
-        octavia.set(gpoSecc, "codigo2");
-        this.update(octavia);
+    public void updateCodigo2(List<GrupoSeccion> gpoSecciones) {
+        StringBuilder sql = new StringBuilder("update GrupoSeccion set codigo2 = case \n");
+        for (GrupoSeccion gs : gpoSecciones) {
+            sql.append(" when id = ").append(gs.getId()).append(" then '").append(gs.getCodigo2()).append("' \n");
+        }
+        sql.append(" end where id in (\n");
+        int loop = 0;
+        for (GrupoSeccion gs : gpoSecciones) {
+            if (loop > 0) {
+                sql.append(",");
+            }
+            sql.append(gs.getId());
+            loop++;
+        }
+        sql.append(")");
+
+        Query query = getCurrentSession().createQuery(sql.toString());
+        query.executeUpdate();
     }
 
 }
