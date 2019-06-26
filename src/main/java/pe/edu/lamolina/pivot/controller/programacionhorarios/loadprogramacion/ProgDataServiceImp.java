d13b2515cf872e92dabf14d68d89d7f0a2e3a7c4
lyon666 <lyon@albatross.pe>
2019-06-26 10:30:58 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/controller/programacionhorarios/loadprogramacion/ProgDataServiceImp.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/programacionhorarios/loadprogramacion/ProgDataServiceImp.java
@@ -1,4 +1,4 @@
-package pe.edu.lamolina.pivot.controller.programacionhorarios.loadprogramacion;
+ package pe.edu.lamolina.pivot.controller.programacionhorarios.loadprogramacion;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
