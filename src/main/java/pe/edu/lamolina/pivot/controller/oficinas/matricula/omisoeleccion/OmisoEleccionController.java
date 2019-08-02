d0ee2a19722c045b74f91f32575b47bcfc08a264
dpcanales <davd.1491@gmail.com>
2019-08-01 19:35:29 -0500

    cambios

--- src/main/java/pe/edu/lamolina/pivot/controller/oficinas/matricula/omisoeleccion/OmisoEleccionController.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/oficinas/matricula/omisoeleccion/OmisoEleccionController.java
@@ -143,7 +143,7 @@ public class OmisoEleccionController {
         DataSessionPivot ds = (DataSessionPivot) session.getAttribute(Constantine.SESSION_USUARIO);
 
         try {
-
+            alumno.getAlumnoOmisoEleccions().get(0).setMotivoAnulacion(alumno.getMotivoAnulacion());
             service.anularOmision(alumno.getAlumnoOmisoEleccions(), ds);
             response.setSuccess(true);
             response.setMessage("Se realizó la actualización satisfactoriamente.");
