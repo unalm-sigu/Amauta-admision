671a046538c69f944a3ef0353c66f934e3f919ab
dpcanales <davd.1491@gmail.com>
2019-08-09 15:59:06 -0500

    cambio

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/infoacademico/InfoAcademicoServiceImpl.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/infoacademico/InfoAcademicoServiceImpl.java
@@ -557,6 +557,7 @@ public class InfoAcademicoServiceImpl implements InfoAcademicoService {
                 "alumnoCicloCurso.creditos",
                 "alumnoCicloCurso.estaAprobado",
                 "alumnoCicloCurso.vecesCursado",
+                "alumnoCicloCurso.vecesCursadoRegular",
                 "alumnoCicloCurso.curso.codigo",
                 "alumnoCicloCurso.curso.nombre",
                 "alumnoCicloCurso.curso.tpc",
@@ -579,7 +580,7 @@ public class InfoAcademicoServiceImpl implements InfoAcademicoService {
         ArrayNode cursosHistoJson = new ArrayNode(JsonNodeFactory.instance);
         for (AlumnoCicloCurso cursoCiclo : cursosCiclos) {
             ObjectNode cursoJson = JsonHelper.createJson(cursoCiclo, JsonNodeFactory.instance, true, new String[]{
-                "estadoEnum", "creditos", "nota", "estaAprobado", "vecesCursado",
+                "estadoEnum", "creditos", "nota", "estaAprobado", "vecesCursado", "vecesCursadoRegular",
                 "curso.codigo",
                 "curso.nombre",
                 "curso.tpc",
