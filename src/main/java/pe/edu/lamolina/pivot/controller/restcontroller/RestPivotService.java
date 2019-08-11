f4d3601266f7779291e35a6656c1dd5546b8a166
dpcanales <davd.1491@gmail.com>
2019-08-11 16:24:33 -0500

    cambios

---
+++ src/main/java/pe/edu/lamolina/pivot/controller/restcontroller/RestPivotService.java
@@ -0,0 +1,9 @@
+package pe.edu.lamolina.pivot.controller.restcontroller;
+
+import pe.edu.lamolina.pivot.zelper.bean.FormImport;
+
+public interface RestPivotService {
+
+    public Boolean validateToken(FormImport json);
+
+}
--- src/main/java/pe/edu/lamolina/pivot/dao/seguridad/hibernate/TokenIngresanteDAOH.java
+++ src/main/java/pe/edu/lamolina/pivot/dao/seguridad/hibernate/TokenIngresanteDAOH.java
@@ -1,8 +1,13 @@
 package pe.edu.lamolina.pivot.dao.seguridad.hibernate;
 
+import java.util.List;
 import org.springframework.stereotype.Repository;
+import pe.albatross.octavia.Octavia;
 import pe.albatross.octavia.easydao.AbstractEasyDAO;
+import pe.edu.lamolina.model.enums.TokenEstadoEnum;
+import pe.edu.lamolina.model.general.Persona;
 import pe.edu.lamolina.model.seguridad.TokenIngresante;
+import pe.edu.lamolina.model.seguridad.Usuario;
 import pe.edu.lamolina.pivot.dao.seguridad.TokenIngresanteDAO;
 
 @Repository
@@ -13,4 +18,15 @@ public class TokenIngresanteDAOH extends AbstractEasyDAO<TokenIngresante> implem
         setClazz(TokenIngresante.class);
     }
 
+    @Override
+    public List<TokenIngresante> allActivos(Persona persona, Usuario usuario) {
+        Octavia sql = Octavia.query()
+                .from(TokenIngresante.class, "too")
+                .join("persona per", "userRegistro user")
+                .filter("per.id", persona)
+                .filter("user.id", usuario)
+                .filter("too.estado", TokenEstadoEnum.ACT);
+
+        return all(sql);
+    }
 }
