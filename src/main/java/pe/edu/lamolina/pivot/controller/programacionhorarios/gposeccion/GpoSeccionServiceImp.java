b382a774b599de2f488d12d8a7c225ef12716566
diesgut <DIEGO@DESKTOP-2TED37V>
2019-08-19 14:01:23 -0500

    cambios modulo de aulas

--- src/main/java/pe/edu/lamolina/pivot/controller/programacionhorarios/gposeccion/GpoSeccionServiceImp.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/programacionhorarios/gposeccion/GpoSeccionServiceImp.java
@@ -2142,6 +2142,7 @@ public class GpoSeccionServiceImp implements GpoSeccionService {
 
         seccionDAO.updateSeccionAula(seccion);
         this.actualizarBoletin();
+        this.actualizarCuotaAnexo(seccion, seccionDB.getGrupoSeccion().getCicloAcademico());
     }
 
     @Override
