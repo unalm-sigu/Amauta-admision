852926fba32eea95dc68e7b1698b34c1143c3fd2
lyon666 <lyon@albatross.pe>
2019-08-21 10:46:46 -0500

    fix

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/tramitesacademicos/tramiteRetiroCiclo/ResponseRestService.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/tramitesacademicos/tramiteRetiroCiclo/ResponseRestService.java
@@ -2,18 +2,19 @@ package pe.edu.lamolina.pivot.controller.academico.tramitesacademicos.tramiteRet
 
 import pe.albatross.zelpers.miscelanea.JsonResponse;
 import pe.edu.lamolina.model.academico.Alumno;
+import pe.edu.lamolina.model.academico.CicloAcademico;
 import pe.edu.lamolina.model.academico.MatriculaResumen;
 import pe.edu.lamolina.pivot.zelper.model.DataSessionPivot;
 
 public interface ResponseRestService {
 
-    public JsonResponse updateRest(MatriculaResumen matriculaResumen, DataSessionPivot ds);
+    JsonResponse updateRest(MatriculaResumen matriculaResumen, DataSessionPivot ds);
 
-    JsonResponse generarAporte(Alumno alumno, MatriculaResumen matriculaResumen, DataSessionPivot ds);
+    JsonResponse generarAporte(Alumno alumno, CicloAcademico ciclo, MatriculaResumen matriculaResumen, DataSessionPivot ds);
 
     void createToken(DataSessionPivot ds);
 
-    public JsonResponse generarAporteCarnet(MatriculaResumen matriculaResumen, DataSessionPivot ds);
+    JsonResponse generarAporteCarnet(MatriculaResumen matriculaResumen, DataSessionPivot ds);
 
-    public JsonResponse eliminarAporteCarnet(MatriculaResumen matriculaResumen, DataSessionPivot ds);
+    JsonResponse eliminarAporteCarnet(MatriculaResumen matriculaResumen, DataSessionPivot ds);
 }
