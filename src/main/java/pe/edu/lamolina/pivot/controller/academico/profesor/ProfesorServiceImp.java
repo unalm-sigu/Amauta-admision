a67223193e5cac6b6ba0fa0c8d2e93281daaacc1
diesgut <DIEGO@DESKTOP-2TED37V>
2019-09-18 14:44:07 -0500

    fix docente

--- src/main/java/pe/edu/lamolina/pivot/controller/academico/profesor/ProfesorServiceImp.java
+++ src/main/java/pe/edu/lamolina/pivot/controller/academico/profesor/ProfesorServiceImp.java
@@ -288,10 +288,13 @@ public class ProfesorServiceImp implements ProfesorService {
             logger.debug("-> Dato basicos de persona actualizados");
         }
 
-        persona.setFoto(personaForm.getFoto());
-        this.uploadS3(personaForm.getFoto());
+        //me indican que solo se debe actualizar foto documento
+        Persona personaUpd = new Persona(personaForm.getId());
+        personaUpd.setRutaFotoDocumento(personaForm.getFullRutaFotoTemporalDocumento());
+       // personaUpd.setRutaFotoTemporal(personaForm.getRutaFotoTemporal()); //     
+        personaDAO.updateColumns(personaUpd, "rutaFotoDocumento");
+        this.uploadS3(personaUpd.getFoto());
 
-        personaDAO.update(persona);
         logger.debug("***Resolviendo en Tabla Docente***");
         Docente docenteBD = docenteDAO.findByDocente(docente);
         docenteBD.setPersona(persona);
