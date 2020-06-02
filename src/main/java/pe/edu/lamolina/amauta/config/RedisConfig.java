5caa38d324caa18327edc162a95f8a93360a7f39
jose-ea <jose@albatross.pe>
2020-06-02 17:27:18 -0500

    fix session timeout

--- src/main/java/pe/edu/lamolina/amauta/config/RedisConfig.java
+++ src/main/java/pe/edu/lamolina/amauta/config/RedisConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 7200)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 43200)
 public class RedisConfig {
 
     @Bean
