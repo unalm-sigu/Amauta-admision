849c2277c8f440716c5f49716992a674c1968c90
david pineda <dpineda@lamolina.edu.pe>
2020-08-15 05:32:49 -0500

    cambios

--- src/main/java/pe/edu/lamolina/amauta/zelper/Laboratory.java
+++ src/main/java/pe/edu/lamolina/amauta/zelper/Laboratory.java
@@ -5,10 +5,8 @@ import java.math.RoundingMode;
 
 public class Laboratory {
 
-    public static void main(String[] args) {
+    public static void main666(String[] args) {
 
-         BigDecimal perAvance = new BigDecimal(500).divide(new BigDecimal(6783),2, RoundingMode.HALF_UP);
-         System.out.println("pe.edu.lamolina.amauta.zelper.Laboratory.main666()" + perAvance);
 
     }
 
