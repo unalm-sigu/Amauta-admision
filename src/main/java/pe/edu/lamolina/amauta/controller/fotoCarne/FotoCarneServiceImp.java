ff52bfb38ca8a4bf408287c643aa9036e6618159
david pineda <dpineda@lamolina.edu.pe>
2020-08-15 01:36:52 -0500

    cambios

--- src/main/java/pe/edu/lamolina/amauta/controller/fotoCarne/FotoCarneServiceImp.java
+++ src/main/java/pe/edu/lamolina/amauta/controller/fotoCarne/FotoCarneServiceImp.java
@@ -120,6 +120,9 @@ public class FotoCarneServiceImp implements FotoCarneService {
                     }
                 }
                 component.setAvance(component.getAvance() + 1);
+                if (component.getAvance() == 50) {
+                    break;
+                }
             }
             component.setEstado("INA");
             out.close();
