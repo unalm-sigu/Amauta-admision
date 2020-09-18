d2ade2da38482f20f891ed91087c4fdb567bc121
david pineda <dpineda@lamolina.edu.pe>
2020-09-18 00:55:05 -0500

    fix

--- src/main/java/pe/edu/lamolina/amauta/controller/academico/cursoPropedeutico/CursoPropedeuticoServiceImp.java
+++ src/main/java/pe/edu/lamolina/amauta/controller/academico/cursoPropedeutico/CursoPropedeuticoServiceImp.java
@@ -162,6 +162,8 @@ public class CursoPropedeuticoServiceImp implements CursoPropedeuticoService {
             acreencia.setFechaRegistro(new Date());
             acreenciaDAO.save(acreencia);
 
+            
+            aporteAlumnoCicloDAO.save(aporteAlumnoCiclo);
         }
 
     }
