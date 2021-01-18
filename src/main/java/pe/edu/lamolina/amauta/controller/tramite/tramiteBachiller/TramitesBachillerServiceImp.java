61e599765337dd4e47f90f77432e2f17125475f6
david pineda <dpineda@lamolina.edu.pe>
2021-01-18 10:31:45 -0500

    cambios

--- src/main/java/pe/edu/lamolina/amauta/controller/tramite/tramiteBachiller/TramitesBachillerServiceImp.java
+++ src/main/java/pe/edu/lamolina/amauta/controller/tramite/tramiteBachiller/TramitesBachillerServiceImp.java
@@ -253,7 +253,7 @@ public class TramitesBachillerServiceImp implements TramitesBachillerService {
         egresado.setCarrera(alumnoDB.getCarrera());
         egresado.setCicloAcademico(ds.getCicloAcademico());
         egresado.setFacultad(alumnoDB.getCarrera().getFacultad());
-        egresado.setEsPrincipal(Integer.MIN_VALUE);
+        egresado.setEsPrincipal(0);
         egresadoDAO.save(egresado);
     }
 
