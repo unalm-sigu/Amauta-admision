847d3db285d419864c671acf03c8e2edade0478b
bladymir <bladymircch@gmail.com>
2021-06-01 08:49:30 -0500

    fix

--- src/main/java/pe/edu/lamolina/amauta/controller/academico/encuestaestudiantil/docentemodalidad/EncuestaDocenteModalidadController.java
+++ src/main/java/pe/edu/lamolina/amauta/controller/academico/encuestaestudiantil/docentemodalidad/EncuestaDocenteModalidadController.java
@@ -64,9 +64,11 @@ public class EncuestaDocenteModalidadController {
         List<Facultad> facultades = departamentos.stream().map(x -> x.getFacultad()).distinct().collect(Collectors.toList());
         ArrayNode jFacultades = JaneHelper.from(facultades).array();
         ArrayNode jDepartamentos = JaneHelper.from(departamentos).join("facultad", "id").array();
+
         model.addAttribute("jFacultades", jFacultades.toString());
         model.addAttribute("jDepartamentos", jDepartamentos.toString());
         model.addAttribute("cicloAcademico", ciclo);
+        model.addAttribute("isDocente", ds.getDocente()!=null);
         return "academico/encuestaestudiantil/docentemodalidad/encuestadocentemodalidad";
     }
 
