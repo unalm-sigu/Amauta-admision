3345cde710ad6b6f2c271e6c143f56a16ea7a482
bladymir <bladymircch@gmail.com>
2021-08-02 16:50:25 -0500

    fix

--- src/main/java/pe/edu/lamolina/amauta/controller/fotocarne/FotoCarneDownloadService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.lamolina.amauta.controller.fotocarne;
-
-import pe.edu.lamolina.amauta.zelper.model.DataSessionPivot;
-
-public interface FotoCarneDownloadService {
-
-    public void compilarInformacion(DataSessionPivot ds, String carrera);
-
-}
