dea4e4b290fe21a410bc2abff1282b01519657c3
bladymir <bladymircch@gmail.com>
2021-08-02 17:26:35 -0500

    super fix

--- src/main/java/pe/edu/lamolina/amauta/controller/fotocarne/FotoCarneUploadService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.lamolina.amauta.controller.fotocarne;
-
-import pe.edu.lamolina.amauta.zelper.model.DataSessionPivot;
-
-public interface FotoCarneUploadService {
-
-    public void procesarFotos(DataSessionPivot ds, String rutaFotos);
-
-}
