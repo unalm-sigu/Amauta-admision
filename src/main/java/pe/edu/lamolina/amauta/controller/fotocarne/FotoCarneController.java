d2529ffb0590693a341b427c318ec2b2db3532c7
bladymir <bladymircch@gmail.com>
2021-08-02 17:28:10 -0500

    super fix

---
+++ src/main/java/pe/edu/lamolina/amauta/controller/fotocarne/FotoCarneController.java
@@ -0,0 +1,208 @@
+package pe.edu.lamolina.amauta.controller.fotocarne;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import static com.helger.commons.io.stream.StreamHelper.close;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pe.albatross.zelpers.json.JaneHelper;
+import pe.albatross.zelpers.miscelanea.ExceptionHandler;
+import pe.albatross.zelpers.miscelanea.JsonResponse;
+import pe.albatross.zelpers.miscelanea.PhobosException;
+import pe.edu.lamolina.amauta.controller.comun.BuscarService;
+import pe.edu.lamolina.amauta.zelper.model.DataSessionPivot;
+import pe.edu.lamolina.model.academico.Carrera;
+import pe.edu.lamolina.model.academico.ModalidadEstudio;
+import pe.edu.lamolina.model.constantines.GlobalConstantine;
+import pe.edu.lamolina.model.enums.ModalidadEstudioEnum;
+import pe.edu.lamolina.model.enums.TipoCarreraEnum;
+
+@Controller
+@RequestMapping("fotos/carne")
+public class FotoCarneController {
+
+    @Autowired
+    FotoCarneDownloadService service;
+
+    @Autowired
+    FotoCarneUploadService fotoCarneUploadService;
+
+    @Autowired
+    BuscarService buscarService;
+
+    @Autowired
+    FotosCarneDown fotosCarneDownComponent;
+
+    @Autowired
+    FotosCarneUpload fotosCarneUploadComponent;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model, HttpSession session) {
+
+        DataSessionPivot ds = (DataSessionPivot) session.getAttribute(GlobalConstantine.SESSION_USUARIO);
+
+        List<ModalidadEstudio> modalidades = buscarService.allModalidadEstudios();
+        ArrayNode modalidadesJson = JaneHelper.from(modalidades).array();
+
+        model.addAttribute("ciclo", ds.getCicloAcademico());
+        model.addAttribute("modalidades", modalidadesJson.toString());
+
+        return "fotoscarne/fotoscarne";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "compilarInformacion/{carrera}")
+    public JsonResponse compilarInformacion(@PathVariable("carrera") String carrera, HttpSession session) {
+
+        JsonResponse response = new JsonResponse();
+
+        try {
+
+            DataSessionPivot ds = (DataSessionPivot) session.getAttribute(GlobalConstantine.SESSION_USUARIO);
+            service.compilarInformacion(ds, carrera);
+            response.setSuccess(true);
+
+        } catch (PhobosException e) {
+            ExceptionHandler.handlePhobosEx(e, response);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, response);
+        }
+
+        return response;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "descargarFotos", method = RequestMethod.GET)
+    public void descargarFotos(HttpServletResponse response) throws IOException {
+
+        if (fotosCarneDownComponent == null) {
+            throw new PhobosException("No se ha iniciado ninguna descarga");
+        }
+
+        if (StringUtils.isBlank(fotosCarneDownComponent.getPathFile())) {
+            throw new PhobosException("No existe la ruta del archivo");
+        }
+
+        logger.debug("{}", fotosCarneDownComponent.getPathFile());
+
+        File filex = new File(fotosCarneDownComponent.getPathFile());
+
+        if (!filex.exists()) {
+            throw new PhobosException("No existe el archivo");
+        }
+
+        if (!filex.exists()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        DateTime hoy = new DateTime();
+
+        response.reset();
+        response.setBufferSize(GlobalConstantine.DEFAULT_BUFFER_SIZE_DOWNLOAD);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=fotos.zip");
+
+        BufferedInputStream input = null;
+        BufferedOutputStream output = null;
+
+        try {
+            input = new BufferedInputStream(new FileInputStream(filex), GlobalConstantine.DEFAULT_BUFFER_SIZE_DOWNLOAD);
+            output = new BufferedOutputStream(response.getOutputStream(), GlobalConstantine.DEFAULT_BUFFER_SIZE_DOWNLOAD);
+            IOUtils.copy(input, output);
+            response.flushBuffer();
+        } finally {
+            close(output);
+            close(input);
+        }
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "infoDown", method = RequestMethod.GET)
+    public ObjectNode infoDown(HttpSession session) {
+        return JaneHelper.from(fotosCarneDownComponent).join("errores").json();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "infoUp", method = RequestMethod.GET)
+    public ObjectNode infoUp(HttpSession session) {
+        return JaneHelper.from(fotosCarneUploadComponent).join("errores").json();
+    }
+
+    @ResponseBody
+    @RequestMapping("procesarFotos")
+    public JsonResponse procesarFotos(@RequestParam("rutaFotos") String rutaFotos, HttpSession session) {
+
+        JsonResponse response = new JsonResponse();
+
+        try {
+
+            DataSessionPivot ds = (DataSessionPivot) session.getAttribute(GlobalConstantine.SESSION_USUARIO);
+            fotoCarneUploadService.procesarFotos(ds, rutaFotos);
+            response.setSuccess(true);
+
+        } catch (PhobosException e) {
+            ExceptionHandler.handlePhobosEx(e, response);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, response);
+        }
+
+        return response;
+
+    }
+
+    @ResponseBody
+    @RequestMapping("allCarreraByModalidad/{modalidad}")
+    public JsonResponse allCarreraByModalidad(@PathVariable("modalidad") String modalidad, HttpSession session) {
+
+        JsonResponse response = new JsonResponse();
+
+        try {
+
+            List<Carrera> carreras = buscarService.allCarrerasActivaByModalidad(modalidad);
+
+            for (Carrera carrera : carreras) {
+                if (carrera.getModalidadEstudio().getCodigoEnum() == ModalidadEstudioEnum.EPG) {
+                    carrera.setNombre((carrera.getTipoEnum().equals(TipoCarreraEnum.MAE) ? "Maestria en " : "Doctorado en ") + carrera.getNombre());
+                }
+            }
+
+            ArrayNode node = JaneHelper.from(carreras).only("id,codigo,nombre").array();
+
+            response.setData(node);
+            response.setTotal(node.size());
+            response.setSuccess(true);
+
+        } catch (PhobosException e) {
+            ExceptionHandler.handlePhobosEx(e, response);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, response);
+        }
+
+        return response;
+    }
+
+}
---
+++ src/main/java/pe/edu/lamolina/amauta/controller/fotocarne/FotoCarneDownloadServiceImp.java
@@ -0,0 +1,187 @@
+package pe.edu.lamolina.amauta.controller.fotocarne;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pe.albatross.zelpers.miscelanea.TypesUtil;
+import pe.edu.lamolina.amauta.dao.academico.MatriculaResumenDAO;
+import pe.edu.lamolina.amauta.zelper.model.DataSessionPivot;
+import pe.edu.lamolina.model.academico.MatriculaResumen;
+import pe.edu.lamolina.model.constantines.GlobalConstantine;
+
+@Service
+@Transactional(readOnly = true)
+public class FotoCarneDownloadServiceImp implements FotoCarneDownloadService {
+
+    @Autowired
+    FotosCarneDown fotosCarneComponent;
+
+    @Autowired
+    MatriculaResumenDAO matriculaResumenDAO;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    @Async
+    public void compilarInformacion(DataSessionPivot ds, String carrera) {
+        logger.debug("inicia descarga foto {}", carrera);
+        if (fotosCarneComponent == null) {
+            logger.debug("componente no creado");
+            fotosCarneComponent = new FotosCarneDown();
+        }
+
+        if (fotosCarneComponent.isIniciado()) {
+            logger.debug("componente esta activo");
+            return;
+        }
+
+        logger.debug("componente no iniciado");
+
+        List<MatriculaResumen> matriculaResumens = matriculaResumenDAO.allMatriculadosByCicloAndCarreraForFoto(ds.getCicloAcademico(), carrera);
+
+        logger.debug("total de matriculas resumen {}", matriculaResumens.size());
+
+        if (matriculaResumens.isEmpty()) {
+            fotosCarneComponent.finalizarProceso();
+            return;
+        }
+
+        fotosCarneComponent.iniciarProceso(matriculaResumens);
+
+        String folder = GlobalConstantine.TMP_DIR + "path_demonium_foto_carnet/";
+
+        File directoryWorkSpace = new File(folder);
+
+        try {
+            FileUtils.deleteDirectory(directoryWorkSpace);
+        } catch (IOException ex) {
+            logger.debug("no existe workspace");
+        }
+
+        directoryWorkSpace.mkdir();
+
+        String hash = TypesUtil.toMD5(System.currentTimeMillis() + "");
+
+        String fotosZipPath = GlobalConstantine.TMP_DIR + "fotos-carnet-" + hash + ".zip";
+
+        File fotosZip = new File(fotosZipPath);
+
+        if (fotosZip.exists()) {
+
+            fotosZip.delete();
+
+        }
+
+        for (MatriculaResumen matriculaResumen : matriculaResumens) {
+
+            String name = matriculaResumen.getAlumno().getCodigo() + ".jpg";
+
+            File file = new File(folder + name);
+            if (StringUtils.isBlank(matriculaResumen.getAlumno().getPersona().getFoto())) {
+
+                fotosCarneComponent.getErrores()
+                        .add(new MsjError("Error descargando foto : " + name));
+                continue;
+            }
+
+            logger.debug("{}", matriculaResumen.getAlumno().getPersona().getFoto());
+
+            try {
+
+                URL url = new URL(matriculaResumen.getAlumno().getPersona().getFoto());
+                FileUtils.copyURLToFile(url, file);
+
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+
+                fotosCarneComponent.getErrores()
+                        .add(new MsjError("Error descargando foto : " + name));
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+
+                fotosCarneComponent.getErrores()
+                        .add(new MsjError("Error descargando foto : " + name));
+
+            }
+
+            fotosCarneComponent.setAvance(fotosCarneComponent.getAvance() + 1);
+
+        }
+
+        comprimirArchivo(fotosZip, folder);
+
+        fotosCarneComponent.setPathFile(fotosZipPath);
+
+        fotosCarneComponent.finalizarProceso();
+
+    }
+
+    private void comprimirArchivo(File fotosZip, String rutaArchivos) {
+
+        File carpetaComprimir = new File(rutaArchivos);
+
+        try {
+
+            if (carpetaComprimir.exists()) {
+
+                ZipOutputStream zous = new ZipOutputStream(new FileOutputStream(fotosZip));
+                File[] archivos = carpetaComprimir.listFiles();
+
+                logger.error("Número de archivos encontrados: {}", archivos.length);
+
+                for (int i = 0; i < archivos.length; i++) {
+                    logger.debug("Nombre del fichero: " + archivos[i].getName());
+                    try {
+
+                        ZipEntry entrada = new ZipEntry(archivos[i].getName());
+                        zous.putNextEntry(entrada);
+
+                        logger.debug("Comprimiendo..... {} ", archivos[i].getName());
+                        FileInputStream file = new FileInputStream(rutaArchivos + entrada.getName());
+                        int leer;
+                        byte[] buffer = new byte[1024];
+                        while (0 < (leer = file.read(buffer))) {
+                            zous.write(buffer, 0, leer);
+                        }
+
+                        file.close();
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                zous.closeEntry();
+                zous.close();
+                logger.error("Directorio de salida: {}", rutaArchivos);
+            }
+
+        } catch (Exception ex) {
+
+            logger.error("Error Descarga de Archivo: {}, fileName: {}", ex, rutaArchivos);
+            fotosCarneComponent.getErrores()
+                    .add(new MsjError("Error comprimiendo archivo: " + rutaArchivos));
+
+        }
+
+    }
+
+}
