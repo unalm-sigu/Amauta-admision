d134deb7b1f42d078f81f8a0d13e82ba54265760
Bladymir <bladymircch@gmail.com>
2021-08-11 14:37:09 -0500

    fix

--- src/main/java/pe/edu/lamolina/amauta/controller/tramite/trasladointerno/TramiteTrasladoService.java
+++ src/main/java/pe/edu/lamolina/amauta/controller/tramite/trasladointerno/TramiteTrasladoService.java
@@ -1,6 +1,7 @@
 package pe.edu.lamolina.amauta.controller.tramite.trasladointerno;
 
 import java.util.List;
+import org.thymeleaf.context.Context;
 import pe.albatross.octavia.dynatable.DynatableFilter;
 import pe.edu.lamolina.amauta.zelper.model.DataSessionPivot;
 import pe.edu.lamolina.model.academico.Carrera;
@@ -13,7 +14,7 @@ public interface TramiteTrasladoService {
 
     public void saveTramiteTraslado(TramiteTraslado traslado, DataSessionPivot ds);
 
-    public String reporte(Tramite tramite, DataSessionPivot ds);
+    public Context reporte(Tramite tramite, DataSessionPivot ds);
 
     public List<Carrera> getCarreras(DataSessionPivot ds);
 
