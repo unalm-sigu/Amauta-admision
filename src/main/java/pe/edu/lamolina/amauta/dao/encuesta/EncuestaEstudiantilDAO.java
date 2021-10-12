dea0812aa0b24d2ffa0835bfcbf5b34839cf837c
Bladymir <bladymircch@gmail.com>
2021-10-12 17:48:00 -0500

    fix

--- src/main/java/pe/edu/lamolina/amauta/dao/encuesta/EncuestaEstudiantilDAO.java
+++ src/main/java/pe/edu/lamolina/amauta/dao/encuesta/EncuestaEstudiantilDAO.java
@@ -1,9 +1,12 @@
 package pe.edu.lamolina.amauta.dao.encuesta;
 
+import java.math.BigInteger;
 import java.util.List;
 import pe.albatross.octavia.easydao.EasyDAO;
 import pe.edu.lamolina.model.academico.CicloAcademico;
 import pe.edu.lamolina.model.encuestaestudiantil.EncuestaEstudiantil;
+import pe.edu.lamolina.model.enums.EncuestaEstudiantilEstadoEnum;
+import pe.edu.lamolina.model.enums.ModalidadEstudioEnum;
 import pe.edu.lamolina.model.enums.TipoExamenVirtualEnum;
 import pe.edu.lamolina.model.examen.ExamenVirtual;
 
@@ -14,5 +17,7 @@ public interface EncuestaEstudiantilDAO extends EasyDAO<EncuestaEstudiantil> {
     EncuestaEstudiantil findByCicloTipo(CicloAcademico cicloAcademico, TipoExamenVirtualEnum tipoEnum);
 
     List<EncuestaEstudiantil> allByEncuestas(List<ExamenVirtual> encuestas);
+
+    BigInteger countEncuestaAlumno(CicloAcademico cicloAcademico, ModalidadEstudioEnum modalidadEstudioEnum, EncuestaEstudiantilEstadoEnum encuestaEstudiantilEstadoEnum);
 
 }
