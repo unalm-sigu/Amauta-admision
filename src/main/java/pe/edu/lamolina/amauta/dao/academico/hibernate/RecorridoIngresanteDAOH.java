e49bb79a078b98ca3d18a1660852f02867b09d88
yuri.paliza <yuri.paliza.contractor@bbva.com>
2024-04-19 11:18:16 -0500

    filtrar ciclo

--- src/main/java/pe/edu/lamolina/amauta/dao/academico/hibernate/RecorridoIngresanteDAOH.java
+++ src/main/java/pe/edu/lamolina/amauta/dao/academico/hibernate/RecorridoIngresanteDAOH.java
@@ -172,7 +172,7 @@ public class RecorridoIngresanteDAOH extends AbstractEasyDAO<RecorridoIngresante
                 .join("cicloAcademico ci", "alumno al")
                 .join("al.persona per", "al.carrera car")
                 .leftJoin("turnoEntrevistaObuae tu", "per.tipoDocumento td")
-                .filter("ci.id", ciclo)
+                .filter("ci.codigo", ciclo.getCodigo())
                 .filter("tu.id", turno)
                 .searchFields("al.codigo", "car.nombre", "per.numeroDocIdentidad", "td.simbolo")
                 .searchComplexField("concat(coalesce(per.paterno,''),' ',coalesce(per.materno,''),' ',coalesce(per.nombres,''))")
