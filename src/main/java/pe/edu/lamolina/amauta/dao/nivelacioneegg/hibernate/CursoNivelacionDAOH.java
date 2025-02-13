836272f3f2addfb09f1f42120f6b3d94ac443fe7
rogelio-orihuela <rolysh-ori@gmail.com>
2025-02-12 20:08:11 -0500

    fix query por codigo o nombre del curso

--- src/main/java/pe/edu/lamolina/amauta/dao/nivelacioneegg/hibernate/CursoNivelacionDAOH.java
+++ src/main/java/pe/edu/lamolina/amauta/dao/nivelacioneegg/hibernate/CursoNivelacionDAOH.java
@@ -43,7 +43,7 @@ public class CursoNivelacionDAOH extends AbstractEasyDAO<CursoNivelacion> implem
                 .join("cuci.curso cu", "cuci.cicloAcademico ci")
                 .leftJoin("aula", "doc.persona per")
                 .filter("ci.id", ciclo)
-                .searchFields("doc.codigo", "cn.codigo", "per.numeroDocIdentidad")
+                .searchFields("doc.codigo", "cn.codigo", "per.numeroDocIdentidad", "cu.codigo", "cu.nombre")
                 .searchComplexField("concat(coalesce(per.paterno,''),' ',coalesce(per.materno,''),' ',coalesce(per.nombres,''))")
                 .searchComplexField("concat(coalesce(per.nombres,''),' ',coalesce(per.paterno,''),' ',coalesce(per.materno,''))")
                 .orderBy("cn.id DESC");
