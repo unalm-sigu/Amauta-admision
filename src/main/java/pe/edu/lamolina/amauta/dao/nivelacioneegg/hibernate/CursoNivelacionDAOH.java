4e828cc7a8697ba4665e268fabf283e74914a31e
RogelioOCL <erorihue712@gmail.com>
2025-10-05 12:45:45 -0500

    update: lista de cursos nivelación de estudios generales

--- src/main/java/pe/edu/lamolina/amauta/dao/nivelacioneegg/hibernate/CursoNivelacionDAOH.java
+++ src/main/java/pe/edu/lamolina/amauta/dao/nivelacioneegg/hibernate/CursoNivelacionDAOH.java
@@ -1,5 +1,6 @@
 package pe.edu.lamolina.amauta.dao.nivelacioneegg.hibernate;
 
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import pe.albatross.octavia.Octavia;
@@ -43,6 +44,7 @@ public class CursoNivelacionDAOH extends AbstractEasyDAO<CursoNivelacion> implem
                 .join("cuci.curso cu", "cuci.cicloAcademico ci")
                 .leftJoin("aula", "doc.persona per")
                 .filter("ci.id", ciclo)
+                .notIn("cn.estado", Arrays.asList(SeccionEstadoEnum.CAN.name()))
                 .searchFields("doc.codigo", "cn.codigo", "per.numeroDocIdentidad", "cu.codigo", "cu.nombre")
                 .searchComplexField("concat(coalesce(per.paterno,''),' ',coalesce(per.materno,''),' ',coalesce(per.nombres,''))")
                 .searchComplexField("concat(coalesce(per.nombres,''),' ',coalesce(per.paterno,''),' ',coalesce(per.materno,''))")
